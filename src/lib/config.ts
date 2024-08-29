5fe7e672434f0a88a2a6ed30a1484fcb5ca89dd9
victoryeo <victoryeo1974@gmail.com>
2024-08-29 14:39:52 +0800

    first commit

---
+++ src/lib/config.ts
@@ -0,0 +1,20 @@
+'use client';
+
+import { http, createStorage, cookieStorage } from 'wagmi';
+import { sepolia, bscTestnet, blastSepolia } from 'wagmi/chains';
+import { Chain, getDefaultConfig } from '@rainbow-me/rainbowkit';
+
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
+console.log(projectId)
+const supportedChains: Chain[] = [ sepolia, bscTestnet, blastSepolia ];
+
+export const config = getDefaultConfig({
+   appName: "private-txn-app",
+   projectId : projectId,
+   chains: supportedChains as any,
+   ssr: true,
+   storage: createStorage({
+    storage: cookieStorage,
+   }),
+  transports: supportedChains.reduce((obj, chain) => ({ ...obj, [chain.id]: http() }), {})
+ });
