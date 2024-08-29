827b1d8dba4cd4d64a9237d6358f775fa5fb75df
victoryeo <victoryeo1974@gmail.com>
2024-08-29 15:36:24 +0800

    fix warning message

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,7 +4,7 @@ import { http, createStorage, cookieStorage } from 'wagmi';
 import { sepolia, bscTestnet, blastSepolia } from 'wagmi/chains';
 import { Chain, getDefaultConfig } from '@rainbow-me/rainbowkit';
 
-const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID!;
 console.log(projectId)
 const supportedChains: Chain[] = [ sepolia, bscTestnet, blastSepolia ];
 
