ea571c4b35deac3578feb8a90e770a701a0ce877
victoryeo <victoryeo1974@gmail.com>
2024-08-29 16:26:06 +0800

    create frontend folder

--- frontend/src/app/components/transfer.tsx
+++ frontend/src/app/components/transfer.tsx
@@ -1,13 +1,39 @@
 import { useState } from "react";
+import { parseEther } from "viem";
+import { useWriteContract } from "wagmi";
+import { erc20Abi } from "@/config/erc20Abi";
+import { zetoAnonAbi } from "@/config/zetoAnonAbi";
 
-const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log('handleSubmit')
-}
+const erc20Address = "0x4a669e1267f6da8b51e21bc3c402a8614ae7cd1a";
+const zetoTokenAddress = "0x26366dd4C51b84490e01233A491Af142a6Ab96Ba";
 
 export const Transfer = () => {
     const [amount, setAmount] = useState("");
     const [recipient, setRecipient] = useState("");
+    const { data: hash, writeContract } = useWriteContract();
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log('handleSubmit', amount, recipient)
+        const amountInWei = parseEther(amount);
+        console.log("amountInWei", amountInWei.toString());
+
+        // Step1: Get approval
+        writeContract({
+            address: erc20Address,
+            abi: erc20Abi,
+            functionName: "approve",
+            args: [zetoTokenAddress, amountInWei.toString()],
+        });
+
+        // Step2: Deposit
+        writeContract({
+            address: zetoTokenAddress,
+            abi: zetoAnonAbi,
+            functionName: "deposit",
+            args: [amount, 0, 0],
+        });
+    }
 
     return (
         <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
