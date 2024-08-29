f5e30d8c114855a6a9ece960714213c19119dee9
victoryeo <victoryeo1974@gmail.com>
2024-08-29 15:43:52 +0800

    extracting the cookie from the request headers within our server component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>{children}</Providers>
+        <Providers cookie={cookie}>{children}</Providers>
       </body>
     </html>
   );
