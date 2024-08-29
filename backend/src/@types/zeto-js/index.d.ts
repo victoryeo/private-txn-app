cceb132c296be65f770d0e54989a3683a1567675
victoryeo <victoryeo1974@gmail.com>
2024-08-29 21:58:45 +0800

    add types declaration

---
+++ backend/src/@types/zeto-js/index.d.ts
@@ -0,0 +1,5 @@
+declare module 'zeto-js' {
+    export function loadCircuit(type: any): any;
+    export function newSalt(): any;
+    export function tokenUriHash(tokenUri: string|undefined): any
+}
