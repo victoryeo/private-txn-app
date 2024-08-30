075d5c707feb50eac9586d03c9a2383cf6f43f3c
victoryeo <victoryeo1974@gmail.com>
2024-08-30 14:53:15 +0800

    update type declaration

--- backend/src/@types/zeto-js/index.d.ts
+++ backend/src/@types/zeto-js/index.d.ts
@@ -3,4 +3,13 @@ declare module 'zeto-js' {
     export function newSalt(): any;
     export function tokenUriHash(tokenUri: string|undefined): any
     export function encodeProof(proofJson: any): any
+
+    export type arrayType = {
+        number: ele[];
+    }
+    export type anyN = {
+        arrayType: pA;
+        arrayType: pB[];
+        arrayType: pC;
+    }
 }
