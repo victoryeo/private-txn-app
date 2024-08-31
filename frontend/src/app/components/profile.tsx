00c03936bd8347307d590871b1f0e68d2d5a8a4e
victoryeo <victoryeo1974@gmail.com>
2024-08-31 13:55:39 +0800

    eth balance

--- frontend/src/app/components/profile.tsx
+++ frontend/src/app/components/profile.tsx
@@ -32,7 +32,7 @@ export default function Profile() {
       </div>
 
       <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-        <h2 className={`mb-3 text-2xl font-semibold`}>Balance</h2>
+        <h2 className={`mb-3 text-2xl font-semibold`}>Eth Balance</h2>
         <div className={`m-0 max-w-[30ch] text-sm opacity-50`}>
           {data ? (
             <p>
